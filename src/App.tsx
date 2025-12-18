4a1e422e677ba2e41a0a7458cb14dfff2839433b
Adam Plesnik <adam.plesnik@gmail.com>
2025-12-18 15:52:57 +0100

    Initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Logo from './components/Logo'
+
 function App() {
-  return <>Silence is golden.</>
+  return (
+    <div className="bg-product-foreground-dim text-neutral-foreground flex h-screen w-screen flex-col items-center justify-center gap-12 p-4 font-mono md:p-8">
+      <Logo />
+      <div className="w-full text-center">Crazy simple climbing grade comparator.</div>
+      <div className="bg-neutral-foreground text-neutral-on-foreground w-fit -rotate-6 p-2 text-center">
+        Beta coming soon.
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <div className="text-7xl leading-16 font-medium">
+      <div>
+        ohmy<span className="text-neutral-on-foreground">*</span>
+      </div>
+      grade
+    </div>
+  )
+}
+
+export default Logo
