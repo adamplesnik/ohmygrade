3aac5c8cdcc93041820a349b76497dfe458ababa
Adam Plesnik <adam.plesnik@gmail.com>
2025-12-18 22:24:50 +0100

    Update logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Logo from './components/Logo'
 
 function App() {
   return (
-    <div className="bg-product-foreground text-product-on-foreground flex h-screen w-screen flex-col items-center justify-center gap-12 p-4 font-mono md:p-8">
+    <div className="bg-product-foreground text-product-on-foreground flex h-screen w-screen flex-col items-center justify-center gap-12 p-6 font-mono md:p-8 lg:p-10">
       <Logo large colorful={false} />
       <div className="w-full text-center">Crazy simple climbing grade comparator.</div>
       <div className="bg-product-on-foreground text-neutral-on-foreground-contrast w-fit -rotate-6 p-2 text-center">
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,9 +8,9 @@ type LogoProps = {
 
 function Logo({ large = false, colorful = true }: LogoProps) {
   return (
-    <div className={clsx(large ? 'text-7xl' : 'text-3xl', 'leading-[80%] font-medium')}>
+    <div className={clsx(large ? 'text-7xl' : 'text-2xl', 'leading-[86%] font-medium tracking-tight')}>
       <div>
-        ohmy<span className={clsx(colorful ? 'text-product-foreground' : 'text-neutral-foreground-dim')}>*</span>
+        oh<span className={clsx(colorful ? 'text-product-foreground' : 'text-neutral-foreground-dim')}>^</span>my
       </div>
       grade
     </div>
