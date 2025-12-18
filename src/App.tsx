951242c1024850a8a1c02bdc6a8b22c4c552a3bd
Adam Plesnik <adam.plesnik@gmail.com>
2025-12-18 16:43:25 +0100

    Add custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Logo from './components/Logo'
 
 function App() {
   return (
-    <div className="bg-product-foreground-dim text-neutral-foreground flex h-screen w-screen flex-col items-center justify-center gap-12 p-4 font-mono md:p-8">
+    <div className="bg-product-foreground text-neutral-foreground flex h-screen w-screen flex-col items-center justify-center gap-12 p-4 font-mono md:p-8">
       <Logo large colorful={false} />
       <div className="w-full text-center">Crazy simple climbing grade comparator.</div>
       <div className="bg-neutral-foreground text-neutral-on-foreground w-fit -rotate-6 p-2 text-center">
