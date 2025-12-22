3b6fda140581ae49a7c086feba0a55dafa58b24e
Adam Plesnik <adam.plesnik@gmail.com>
2025-12-22 16:27:57 +0100

    Button, Header components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,17 @@ import { HTMLAttributes } from 'react'
 
 type ButtonProps = {
   value: string
+  active?: boolean
   big?: boolean
 } & HTMLAttributes<HTMLDivElement>
 
-function Button({ value, big }: ButtonProps) {
+function Button({ value, big, active }: ButtonProps) {
   return (
     <div
       className={clsx(
         'hover:border-neutral-foreground active:border-neutral-foreground flex cursor-pointer items-center border border-transparent font-semibold transition-colors',
-        big ? 'h-12 px-4' : 'h-8 px-2'
+        big ? 'h-12 px-4' : 'h-8 px-2',
+        active ? 'bg-neutral-foreground text-neutral-on-foreground' : ''
       )}
     >
       {value}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import Logo from './Logo'
 
 function Header() {
   return (
-    <div className="bg-neutral-background/70 sticky top-0 flex items-center pt-6 pb-4 md:pt-8 lg:pt-10">
+    <div className="bg-neutral-background/70 sticky top-0 z-10 flex items-center pt-6 pb-4 md:pt-8 lg:pt-10">
       <Logo />
       <div className="z-10 flex flex-1 items-center justify-end gap-2 md:justify-center">
-        <Button value="Table" big />
+        <Button value="Table" big active />
         <Button value="Comparator" big />
       </div>
       <div className="hidden justify-end gap-1 md:flex">
