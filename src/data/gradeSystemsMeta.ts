3b6076a488a19a7ff8c692abd94b4acd63797502
Adam Plesnik <adam.plesnik@gmail.com>
2025-12-22 16:23:16 +0100

    Update, rename data

---
+++ src/data/gradeSystemsMeta.ts
@@ -0,0 +1,31 @@
+import { GradeSystemMetaType } from '../types/grade.types'
+
+const gradeSystemsMeta: GradeSystemMetaType[] = [
+  {
+    system: 'ew',
+    name: 'EW',
+    type: 'climb',
+  },
+  {
+    system: 'uk',
+    name: 'UK',
+    type: 'climb',
+  },
+  {
+    system: 'uiaa',
+    name: 'UIAA',
+    type: 'climb',
+  },
+  {
+    system: 'yds',
+    name: 'YDS',
+    type: 'climb',
+  },
+  {
+    system: 'fr',
+    name: 'French',
+    type: 'climb',
+  },
+]
+
+export default gradeSystemsMeta
--- src/types/grade.types.ts
+++ src/types/grade.types.ts
@@ -1,13 +1,13 @@
 export type GradeSystemId = 'ew' | 'yds' | 'uiaa' | 'fr' | 'uk'
 
-export type GradeRange = {
+export type GradeRangeType = {
   system: GradeSystemId
   value: string
   start: number
   end: number
 }
 
-export type GradeSystemMeta = {
+export type GradeSystemMetaType = {
   system: GradeSystemId
   name: string
   type: 'climb' | 'boulder'
