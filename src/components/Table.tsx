4870310a3d2a48da0a691a2dbb2fbff79263da3d
Adam Plesnik <adam.plesnik@gmail.com>
2026-01-09 15:39:54 +0100

    Rename

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,7 +3,7 @@ import { useMemo, useState } from 'react'
 import grades from '../data/grades.ts'
 import gradeSystemsMeta from '../data/gradeSystemsMeta.ts'
 import { GRADE_UI_RATIO } from '../helpers/constants.ts'
-import { getGradesBySystem } from '../helpers/getGradesBySystem.ts'
+import { getGradesBySystem } from '../helpers/grades.ts'
 import { getOverlap, getOverlapClass, getOverlapRatio, getOverlapStrength } from '../helpers/overlaps.ts'
 import { GradeRangeType } from '../types/grade.types.ts'
 
@@ -17,7 +17,7 @@ export default function Table() {
       {gradeSystemsMeta.map((system) => (
         <div key={system.system} className="group w-50">
           <div className="bg-neutral-background-alt border-neutral-foreground sticky top-24 z-100 border-b px-4 py-2">
-            <h3 className="font-semibold">{system.name}</h3>
+            <h3 className="group-hover:text-product-foreground font-semibold">{system.name}</h3>
             <div className="text-neutral-foreground-dim -mt-0.5 text-xs">{system.type}</div>
           </div>
           <div className="font-mono">
@@ -46,14 +46,7 @@ export default function Table() {
                     )}
                   >
                     <div className="flex-1">{grade.value}</div>
-                    {isClicked && (
-                      <div
-                        className={clsx(
-                          'bg-neutral-foreground flex size-2 rounded-full',
-                          getOverlapClass(getOverlapStrength(isClicked, clickedOverlapRatio), 'opacity')
-                        )}
-                      ></div>
-                    )}
+                    {isClicked && <div className={clsx('bg-neutral-foreground flex size-2 rounded-full')}></div>}
                   </div>
                 </div>
               )
