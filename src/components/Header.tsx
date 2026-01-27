f731d011a452fe2c05dc30c4fb20c59c96e23975
Adam Plesnik <adam.plesnik@gmail.com>
2026-01-27 10:52:19 +0100

    Add favicons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="bg-neutral-background-alt/80 fixed top-0 right-0 left-0 z-10 h-36 px-4 pt-8 backdrop-blur-xs md:px-8 lg:px-10">
+      <header className="bg-neutral-background-alt/80 fixed top-0 left-0 z-10 h-36 w-screen px-4 pt-8 backdrop-blur-xs md:px-8 lg:px-10">
         <div className="flex items-center gap-8 pl-4">
           <Logo />
           <div className="text-neutral-main-dim hidden sm:block">{t('title')}</div>
