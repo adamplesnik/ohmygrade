44d8dbda74683bba0674264ba5350340bedd58cb
Adam Plesnik <adam.plesnik@gmail.com>
2026-02-02 14:21:20 +0100

    Remove google fonts, keep default system fonts

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ children, active, onClick }: ButtonProps) => {
   return (
     <div
       className={clsx(
-        'active:bg-neutral-container-bright hover:bg-neutral-container flex h-10 cursor-pointer items-center rounded-full border border-transparent px-4 font-semibold transition-colors',
+        'active:bg-neutral-container hover:bg-neutral-container flex h-10 cursor-pointer items-center rounded-full border border-transparent px-4 font-semibold transition-colors',
         active ? 'bg-neutral-main text-neutral-on-main' : ''
       )}
       onClick={onClick}
