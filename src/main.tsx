28b81ec52678dba8c7d462db04ac970420460fcf
Adam Plesnik <adam.plesnik@gmail.com>
2026-02-02 14:36:01 +0100

    Add index.css back to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ActiveSystemsProvider } from './context/active-systems/ActiveSystemsProvider.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
